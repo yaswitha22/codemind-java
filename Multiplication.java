42b0db289bc6bcbe5aedd73b6f7035d85d8038b6
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-08-29 11:09:05 +0530

    import java.util.Scanner; class multiply {     public static void main(String y[])     {         int a,b,mul;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         mul=a*b;         System.out.println(mul);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class multiply
+{
+    public static void main(String y[])
+    {
+        int a,b,mul;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        mul=a*b;
+        System.out.println(mul);
+    }
+}
