5ff346455a1f8e31f287d4eadfa1f35c1b9194ab
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-09-02 10:27:59 +0530

    import java.util.Scanner; class inp {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s;         s=sc.nextLine();         System.out.println(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class inp
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s;
+        s=sc.nextLine();
+        System.out.println(s);
+    }
+}
