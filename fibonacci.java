0fb8707cd37e8cdd41358bb2a56204f1c61b42ee
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-09-18 22:23:56 +0530

    import java.util.Scanner; class fibonacci {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a=0,b=1,c;         n=sc.nextInt();         for(int i=1;i<=n;i++)         {             System.out.print(a+" ");             c=a+b;             a=b;             b=c;         }     } }

---
+++ fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class fibonacci
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a=0,b=1,c;
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.print(a+" ");
+            c=a+b;
+            a=b;
+            b=c;
+        }
+    }
+}
