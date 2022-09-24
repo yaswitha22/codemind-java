e3e70a6f3f5f3bd559ca0f5508aacd67451c6789
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-09-24 13:57:34 +0530

    import java.util.Scanner; class smallfact {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int t;         t=sc.nextInt();         for(int i=0;i<t;i++)         {             int n,sf=1;             n=sc.nextInt();             for(int j=1;j<=n;j++)             {                 sf*=j;             }             System.out.println(sf);         }     } }

---
+++ Small_Factorials.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class smallfact
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t;
+        t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n,sf=1;
+            n=sc.nextInt();
+            for(int j=1;j<=n;j++)
+            {
+                sf*=j;
+            }
+            System.out.println(sf);
+        }
+    }
+}
