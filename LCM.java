56454992b9a20718d0b0e9004a630411ac5e74c2
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-09-26 20:27:48 +0530

    import java.util.Scanner; class lcm {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,gcd=0;         a=sc.nextInt();         b=sc.nextInt();         for(int i=1;i<=b;i++)         {             if(a%i==0 && b%i==0)             {                 gcd=i;             }         }         System.out.print((a*b)/gcd);     } }

---
+++ LCM.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class lcm
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,gcd=0;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(int i=1;i<=b;i++)
+        {
+            if(a%i==0 && b%i==0)
+            {
+                gcd=i;
+            }
+        }
+        System.out.print((a*b)/gcd);
+    }
+}
