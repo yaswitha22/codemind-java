5074a0ac17cbf97290de3bd57178e71242040ba5
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-10-01 11:45:59 +0530

    import java.util.Scanner; class occurances {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         char ch=sc.next().charAt(0);         int k=0;         for(int i=0;i<s.length();i++)         {             if(s.charAt(i)==ch)             {                 k++;             }         }         if(k==0)         System.out.print("-1");         else         System.out.print(k);     } }

--- counting_the_occurances.java
+++ counting_the_occurances.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
-class numbrs
+class occurances
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
-        int k=0;
         char ch=sc.next().charAt(0);
+        int k=0;
         for(int i=0;i<s.length();i++)
         {
-            char c=s.charAt(i);
-            if(c==ch)
+            if(s.charAt(i)==ch)
             {
                 k++;
             }
