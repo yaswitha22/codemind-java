8c7c8048ab07eb35905bbcf233a56a529f17e4eb
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-10-08 12:32:54 +0530

    import java.util.Scanner; class matrix {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m=sc.nextInt();         int n=sc.nextInt();         int sum=0;         int a[][]=new int[m][n];         for(int i=0;i<m;i++)         {             for(int j=0;j<n;j++)             {                 a[i][j]=sc.nextInt();             }         }         for(int i=0;i<m;i++)         {             for(int j=0;j<n;j++)             {                sum+=a[i][j];             }         }         System.out.print(sum);     } }

---
+++ Sum_of_matrix_elements.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class matrix
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int sum=0;
+        int a[][]=new int[m][n];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                a[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+               sum+=a[i][j];
+            }
+        }
+        System.out.print(sum);
+    }
+}
