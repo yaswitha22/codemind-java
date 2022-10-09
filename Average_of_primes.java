44dddda8eaabff4ccf3a21fb0045415f95cedf7d
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-10-09 13:44:21 +0530

    import java.util.Scanner; class xyz {     public static boolean prime(int n)     {         if(n<2)         {             return false;         }         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             {                 return false;             }         }         return true;     }     public static void main(String args[])     {         int n,a[],i;         double s=0,c=0,avg;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(prime(a[i]))             {                 s+=a[i];                 c+=1;             }         }         avg=s/c;         System.out.format("%.2f",avg);     } }

---
+++ Average_of_primes.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class xyz
+{
+    public static boolean prime(int n)
+    {
+        if(n<2)
+        {
+            return false;
+        }
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        int n,a[],i;
+        double s=0,c=0,avg;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(prime(a[i]))
+            {
+                s+=a[i];
+                c+=1;
+            }
+        }
+        avg=s/c;
+        System.out.format("%.2f",avg);
+    }
+}
