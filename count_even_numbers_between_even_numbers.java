c22bc9e6df51f1b155045e3330ef765f1851186d
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-10-13 20:17:36 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a[],c=0;         n=sc.nextInt();         a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n-2;i++)         {             if(a[i]%2==0 && a[i+2]%2==0 && a[i+1]%2==0)             c++;         }         System.out.println(c);
        }
    }

---
+++ count_even_numbers_between_even_numbers.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a[],c=0;
+        n=sc.nextInt();
+        a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-2;i++)
+        {
+            if(a[i]%2==0 && a[i+2]%2==0 && a[i+1]%2==0)
+            c++;
+        }
+        System.out.println(c);
+        
+    }
+}
