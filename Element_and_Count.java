918085e1520ba532e09305550096b3d5c4ec3284
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-10-14 20:18:44 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a[],c=0,k=0;         n=sc.nextInt();         a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             c=0;             if(a[i]==-1)             continue;             else             {            for(int j=0;j<n;j++)            {
                   if(a[i]==a[j]&& i!=j)
                   {
                   a[j]=-1;
                   c++;
                   }
               }
               System.out.print(a[i]+" "+(c+1)+" ");
               }
            }
        }
    }

---
+++ Element_and_Count.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a[],c=0,k=0;
+        n=sc.nextInt();
+        a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            c=0;
+            if(a[i]==-1)
+            continue;
+            else
+            {
+           for(int j=0;j<n;j++)
+           {
+               
+               if(a[i]==a[j]&& i!=j)
+               {
+               a[j]=-1;
+               c++;
+               }
+           }
+           System.out.print(a[i]+" "+(c+1)+" ");
+           }
+        }
+    }
+}
