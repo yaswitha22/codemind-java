f50b55bc10552a6d7ea8db9ea4c3f92ea0fec908
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-11-02 00:19:48 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         System.out.println(s.replace(".","[.]"));     } }

---
+++ Defanging_an_IP_Address.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s.replace(".","[.]"));
+    }
+}
