65ce8d941742bff2098fb8093c0731312e74c3b9
Yaswitha <97159770+yaswitha22@users.noreply.github.com>
2022-11-02 00:24:00 +0530

    import java.util.*; class thub {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         System.out.println("Hello Technicalhub");         System.out.print(s);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class thub
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.print(s);
+    }
+}
